8dbd225667285d824723ffefddc14fb24521f800
Vikku Nair <vikku@Vikkus-MacBook-Pro.local>
2022-10-26 11:15:30 +0100

    Everything maskes sense

---
+++ commit_test.py
@@ -0,0 +1,21 @@
+class MyFirstClass:
+    pass
+
+
+class MySecondClass:
+    pass
+
+
+def top_level_function():
+    return None
+
+
+# def test():
+#     a = b                                                                                                                       =1
+
+#     return True
+
+def test():
+    a = b                                                                                                                       =1
+
+    return True
